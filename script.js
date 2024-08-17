496e33ae150644f08124374f494dce0ebc436107
ultratoc <ultratocbusiness@gmail.com>
2024-08-17 03:35:27 +0000

    Update script.js for ok

--- script.js
+++ script.js
@@ -1,41 +1,26 @@
 //var gaf = 'AIzaSyDr3YaJjFL8TXoKid6EhT6OuXICrDxVlk8'
-var gaf = 'AIzaSyBnAa8ZvWoXucHYNn8J5LjKR5L-viCcnY8'
-var video = 'u_Wbj5wTvN0'
+var gaf = 'AIzaSyDr3YaJjFL8TXoKid6EhT6OuXICrDxVlk8'
+var gafs = ['AIzaSyBnAa8ZvWoXucHYNn8J5LjKR5L-viCcnY8', 'AIzaSyBRWJwIp50Ll9VjTD5pAjt_6mlb_9UtZss'];
+var gc = 0;
+var video = 'oy9ghhZ0tFs'
 var url = "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&key=" + gaf + "&videoId=" + video + "&maxResults=100"
 
 var display = document.getElementById("display")
+var displayL = document.getElementById("displayL")
 var nameEL = document.getElementById("name")
 
 var comments = []
 var table = {
-  "A" : "10 Dollar Gift Card",
-  "B" : "Airplane Seat",
-  "C" : "Arcade Machine",
-  "D" : "Chezburger",
-  "E" : "Coconut Water",
-  "F" : "Confetti Canon",
-  "G" : "Coupony",
-  "H" : "Credit Card",
-  "I" : "Daisy",
-  "J" : "Dart Board",
-  "K" : "Edd",
-  "L" : "Fake Fake Fake Candle",
-  "M" : "Flexatone",
-  "N" : "Gleepus",
-  "O" : "Green Egg Toast",
-  "P" : "Handheld Air Raid Siren",
-  "Q" : "Honey In A Bear Shaped Jar",
-  "R" : "Mega's Prediction Layout",
-  "S" : "My Best Friend",
-  "T" : "Pan",
-  "U" : "Portable Tv",
-  "V" : "Rubber Band Ball",
-  "W" : "Shiny Charm",
-  "X" : "Sketchbook",
-  "Y" : "Stackable Chairs",
-  "AND" : "Tech Deck",
-  "Z" : "Toliet Paper",
-  "3" : "Hedge"
+  "A": "Cheese Plate",
+  "B": "Garlic Bread",
+  "C": "Banana Slice",
+  "D": "Ham And Cheese Crackers",
+  "E": "DVD",
+  "1": "Cheese Plate",
+  "2": "Garlic Bread",
+  "3": "Banana Slice",
+  "4": "Ham And Cheese Crackers",
+  "5": "DVD"
 }
 
 function commentCrawl(token) {
@@ -55,6 +40,9 @@ function commentCrawl(token) {
 
     } else {
       console.log(`Sad Error: ${xhr.status}`);
+      gaf = gafs[gc];
+      g++;
+      commentCrawl(token);
     }
   };
 }
@@ -77,10 +65,24 @@ function postProcess() {
 
     raw = comment.snippet.topLevelComment.snippet.textOriginal.toUpperCase()
 
-    votes = [...raw.matchAll(new RegExp("\\[[a-zA-Z0-9]+\\]", 'gi'))][0]
-
-    if (votes == undefined || votes.length > 1) {
+    let votes_raw = [...raw.matchAll(new RegExp("\\[[a-zA-Z0-9]+\\]", 'gi'))]
+    let votes = [];
+    votes_raw.forEach((v) => votes.push(v[0]));
+    if (votes == undefined || votes.length > 2) {
       continue
+    } else if (votes.length == 2) {
+      let nt1 = isNaN(parseInt(votes[0].replace('[', '').replace(']', '')));
+      let nt2 = isNaN(parseInt(votes[1].replace('[', '').replace(']', '')));
+      if ((nt1 || nt2) && !(nt1 && nt2)) {
+        for (let v of votes) {
+          if (!flags.includes(v)) {
+            flags.push(v);
+            counts[v] = 0
+          }
+
+          counts[v]++
+        }
+      }
     }
     else {
       if (!flags.includes(votes[0])) {
@@ -93,25 +95,34 @@ function postProcess() {
   }
 
   let displayText = ""
-  
+  let displayLikes = ""
+
   counts = Object.keys(counts)
     .sort((a, b) => counts[b] - counts[a])
     .reduce((acc, key) => {
       acc[key] = counts[key];
       return acc;
     }, {});
-  
+
   for (let key of Object.keys(counts)) {
     clean_key = key.replace('[', '').replace(']', '')
-    if (Object.keys(table).includes(clean_key)) clean_key = table[clean_key]
-    displayText += clean_key + ": " + counts[key] + "\n"
+    if (Object.keys(table).includes(clean_key)) {
+      let entry = table[clean_key] + ": " + counts[key] + "\n"
+      if (isNaN(parseInt(clean_key))) {
+        displayText += entry
+      } else {
+        displayLikes += entry
+      }
+    }
+
   }
 
   display.innerText = displayText
+  displayL.innerText = displayLikes
 }
 
 function startProcess() {
-  video = document.getElementById("videoID").value
+  //video = document.getElementById("videoID").value
   url = "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&key=" + gaf + "&videoId=" + video + "&maxResults=100"
   getName()
   document.getElementById("videoID").style.display = "none"
@@ -135,6 +146,10 @@ function getName() {
       console.log("goop")
     } else {
       console.log(`Sad Error: ${xhr.status} ${xhr.response}`);
+      gaf = gafs[gc];
+      g++;
+      getName();
     }
   };
 }
+startProcess();
