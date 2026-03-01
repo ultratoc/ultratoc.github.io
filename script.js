db54a193027466fac32f9ab6f31ba25a979d7053
ultratoc <ultratocbusiness@gmail.com>
2026-03-01 06:47:07 +0000

    Update script.js for ep3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var gaf = 'AIzaSyDr3YaJjFL8TXoKid6EhT6OuXICrDxVlk8'
 var gafs = ['AIzaSyBnAa8ZvWoXucHYNn8J5LjKR5L-viCcnY8', 'AIzaSyBRWJwIp50Ll9VjTD5pAjt_6mlb_9UtZss'];
 var gc = 0;
-var video = 'R3JNq1W7VoE';
+var video = 'f3tGKZoBcSY';
 var url = "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&key=" + gaf + "&videoId=" + video + "&maxResults=100"
 var video_uploaded = null;
 const MS_48_HOURS = 172800000;
@@ -13,11 +13,12 @@ var nameEL = document.getElementById("name")
 var tagged_votes = [];
 var comments = []
 var table = {
-   "A": "Xylophone",
-   "B": "Rusty Pipe",
-   "C": "Piñata",
-   "D": "Propeller Hat",
-   "E": "Gummy Worm"
+   "A": "Dresser",
+   "B": "Cinderblock",
+   "C": "Birdhouse",
+   "D": "Lamp",
+   "E": "Alarm Clock",
+   "F": "Sunnyside"
 }
 
 function commentCrawl(token) {
